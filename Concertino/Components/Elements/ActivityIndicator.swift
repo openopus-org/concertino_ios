0977a6331b846de785d8775d7b7916227f2480da
Open Opus <concertino@openopus.org>
2020-02-08 00:52:50 -0300

    Refined: composer search, tab navigation

---
+++ Concertino/Components/Elements/ActivityIndicator.swift
@@ -0,0 +1,21 @@
+//
+//  ActivityIndicator.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 07/02/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActivityIndicator_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityIndicator()
+    }
+}
---
+++ Concertino/Components/Elements/ComposerBox.swift
@@ -0,0 +1,21 @@
+//
+//  ComposerBox.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 07/02/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ComposerBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ComposerBox_Previews: PreviewProvider {
+    static var previews: some View {
+        ComposerBox()
+    }
+}
