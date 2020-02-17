c3966210d66e805e32f63c7402d799caca23db8f
Open Opus <concertino@openopus.org>
2020-02-16 22:50:18 -0300

    Recording detail page; several data model changes

---
+++ Concertino/Components/RecordingDetail.swift
@@ -0,0 +1,21 @@
+//
+//  RecordingDetail.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 16/02/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct RecordingDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordingDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordingDetail()
+    }
+}
