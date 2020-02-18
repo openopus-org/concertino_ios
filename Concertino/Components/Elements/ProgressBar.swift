ecd0ec1cfefda725f649794f81601365e0f233e7
Open Opus <concertino@openopus.org>
2020-02-18 14:09:59 -0300

    Player bar enhancements

---
+++ Concertino/Components/Elements/ProgressBar.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressBar.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 18/02/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressBar()
+    }
+}
