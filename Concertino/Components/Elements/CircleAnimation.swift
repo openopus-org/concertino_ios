daa4d67fbef2a8ead5512a98d3e28ec6dd9b5bad
Open Opus <concertino@openopus.org>
2020-02-25 00:04:18 -0300

    Recording page detects if it's already playing

---
+++ Concertino/Components/Elements/CircleAnimation.swift
@@ -0,0 +1,21 @@
+//
+//  CircleAnimation.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 24/02/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleAnimation_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleAnimation()
+    }
+}
---
+++ Concertino/Components/Elements/DotsAnimation.swift
@@ -0,0 +1,21 @@
+//
+//  DotsAnimation.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 24/02/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct DotsAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DotsAnimation_Previews: PreviewProvider {
+    static var previews: some View {
+        DotsAnimation()
+    }
+}
