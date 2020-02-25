2cc74bd3681e730f66aa629f4edf6d85d2802da7
Open Opus <concertino@openopus.org>
2020-02-24 21:19:08 -0300

    Recording panel adjustments, MusicKit refinement

---
+++ Concertino/Components/Elements/RecordingPlaybackControl.swift
@@ -0,0 +1,21 @@
+//
+//  RecordingPlaybackControl.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 24/02/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct RecordingPlaybackControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordingPlaybackControl_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordingPlaybackControl()
+    }
+}
---
+++ Concertino/Components/Elements/RecordingProgressBars.swift
@@ -0,0 +1,21 @@
+//
+//  RecordingProgressBars.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 24/02/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct RecordingProgressBars: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordingProgressBars_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordingProgressBars()
+    }
+}
