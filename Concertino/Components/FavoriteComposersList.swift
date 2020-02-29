e8e4114de6e6820e4d874424b524fa7f48737d71
Open Opus <concertino@openopus.org>
2020-02-29 02:03:23 -0300

    Login, Auth, Favorite composers

---
+++ Concertino/Components/FavoriteComposersList.swift
@@ -0,0 +1,21 @@
+//
+//  FavoriteComposersList.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 29/02/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoriteComposersList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteComposersList_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteComposersList()
+    }
+}
