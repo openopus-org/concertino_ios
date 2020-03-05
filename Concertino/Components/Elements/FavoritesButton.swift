2027d5d098df95c3055bc6362c65991bc45229ce
Open Opus <concertino@openopus.org>
2020-03-05 01:09:31 -0300

    Playlists, favorites, recently played screens

---
+++ Concertino/Components/Elements/FavoritesButton.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesButton.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 04/03/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoritesButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoritesButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesButton()
+    }
+}
---
+++ Concertino/Components/PlaylistsMenu.swift
@@ -0,0 +1,21 @@
+//
+//  PlaylistsMenu.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 04/03/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlaylistsMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaylistsMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaylistsMenu()
+    }
+}
