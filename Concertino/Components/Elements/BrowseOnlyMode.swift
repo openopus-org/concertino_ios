5db5a0f55029e31cc3a8bd3de5e2bd9236cedfd7
Open Opus <concertino@openopus.org>
2020-03-07 01:12:10 -0300

    No miniplayer for guests, Browse-only mode

---
+++ Concertino/Components/Elements/BrowseOnlyMode.swift
@@ -0,0 +1,21 @@
+//
+//  BrowseOnlyMode.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 07/03/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct BrowseOnlyMode: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BrowseOnlyMode_Previews: PreviewProvider {
+    static var previews: some View {
+        BrowseOnlyMode()
+    }
+}
