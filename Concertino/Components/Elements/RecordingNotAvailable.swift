06da67153fb73dba051f02efd5d7048a9e64d37d
Open Opus <concertino@openopus.org>
2020-03-15 21:24:00 -0300

    Effectively detect if player can't start the music

---
+++ Concertino/Components/Elements/RecordingNotAvailable.swift
@@ -0,0 +1,21 @@
+//
+//  RecordingNotAvailable.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 15/03/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct RecordingNotAvailable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordingNotAvailable_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordingNotAvailable()
+    }
+}
