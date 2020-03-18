567e3743c521247035e1ad13501ffbc68cbadf0b
Open Opus <concertino@openopus.org>
2020-03-18 01:30:47 -0300

    Playlist radio; Radio and player improvements

---
+++ Concertino/Components/Elements/PlaylistButtons.swift
@@ -0,0 +1,21 @@
+//
+//  PlaylistButtons.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 17/03/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlaylistButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaylistButtons_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaylistButtons()
+    }
+}
