cc118eacbdf4bba76d7958d27fbcd91786e33110
Open Opus <concertino@openopus.org>
2020-03-19 22:04:16 -0300

    Playlist radio buttons

--- Concertino/Components/Elements/PlaylistButtons.swift
+++ Concertino/Components/Elements/PlaylistButtons.swift
@@ -20,20 +20,27 @@ struct PlaylistButtons: View {
         HStack(spacing: 6) {
             Button(
                 action: {
-                    var recs = self.recordings
-                    recs.shuffle()
-                    
-                    self.radioState.isActive = true
-                    self.radioState.playlistId = self.playlistId
-                    self.radioState.nextWorks.removeAll()
-                    self.radioState.nextRecordings = recs
-                    
-                    let rec = self.radioState.nextRecordings.removeFirst()
-                    
-                    getRecordingDetail(recording: rec, country: self.settingStore.country) { recordingData in
-                        DispatchQueue.main.async {
-                            self.playState.autoplay = true
-                            self.playState.recording = recordingData
+                    if self.radioState.isActive && self.radioState.playlistId == self.playlistId {
+                        self.radioState.isActive = false
+                        self.radioState.playlistId = ""
+                        self.radioState.nextWorks.removeAll()
+                        self.radioState.nextRecordings.removeAll()
+                    } else {
+                        var recs = self.recordings
+                        recs.shuffle()
+                        
+                        self.radioState.isActive = true
+                        self.radioState.playlistId = self.playlistId
+                        self.radioState.nextWorks.removeAll()
+                        self.radioState.nextRecordings = recs
+                        
+                        let rec = self.radioState.nextRecordings.removeFirst()
+                        
+                        getRecordingDetail(recording: rec, country: self.settingStore.country) { recordingData in
+                            DispatchQueue.main.async {
+                                self.playState.autoplay = true
+                                self.playState.recording = recordingData
+                            }
                         }
                     }
                 },
@@ -42,23 +49,21 @@ struct PlaylistButtons: View {
                         HStack {
                             Spacer()
                             
-                            Image("radio")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(height: 18)
+                            AnimatedRadioIcon(color: Color(hex: 0xFFFFFF), isAnimated: self.radioState.isActive && self.radioState.playlistId == self.playlistId)
+                                .frame(width: 40, height: 20)
+                                .padding(.trailing, self.radioState.isActive && self.radioState.playlistId == self.playlistId ? 3 : -10)
+                                .padding(.leading, self.radioState.isActive && self.radioState.playlistId == self.playlistId ? 0 : -10)
+                                
+                            Text((self.radioState.isActive && self.radioState.playlistId == self.playlistId ? "stop radio" : "start radio").uppercased())
                                 .foregroundColor(.white)
-                                .padding(.trailing, 3)
-                            
-                            Text("start radio".uppercased())
-                                .foregroundColor(.white)
-                                .font(.custom("Nunito", size: 13))
+                                .font(.custom("Nunito", size: self.radioState.isActive && self.radioState.playlistId == self.playlistId ? 11 : 13))
                             
                             Spacer()
                         }
                     }
                     .padding(13)
                     .foregroundColor(.white)
-                    .background(Color(hex: 0xfe365e))
+                    .background(Color(hex: self.radioState.isActive && self.radioState.playlistId == self.playlistId ? 0x696969 : 0xfe365e))
                     .cornerRadius(16)
             })
             .buttonStyle(BorderlessButtonStyle())
