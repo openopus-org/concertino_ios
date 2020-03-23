1c7d693ae30f63e2dbf42effcc49567636c48798
Open Opus <concertino@openopus.org>
2020-03-23 01:10:34 -0300

    Composer detail: favorite and forbidden

---
+++ Concertino/Components/Elements/EllipsisButton.swift
@@ -0,0 +1,21 @@
+//
+//  EllipsisButton.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 22/03/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct EllipsisButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EllipsisButton_Previews: PreviewProvider {
+    static var previews: some View {
+        EllipsisButton()
+    }
+}
