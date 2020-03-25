fc88dcacfcbd27bf0e8a3e71a9d3f080bc5896e2
Open Opus <concertino@openopus.org>
2020-03-25 00:32:52 -0300

    Add recordings to favorites; Share sheet

---
+++ Concertino/Components/Elements/ShareButton.swift
@@ -0,0 +1,21 @@
+//
+//  ShareButton.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 24/03/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShareButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShareButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ShareButton()
+    }
+}
