7b8f500984065dad2e95fe92f51361d2d609c3f9
Open Opus <concertino@openopus.org>
2020-03-26 00:38:24 -0300

    Add to playlist; Create new playlist

---
+++ Concertino/Components/Elements/PlaylistChooser.swift
@@ -0,0 +1,21 @@
+//
+//  PlaylistChooser.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 25/03/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlaylistChooser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaylistChooser_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaylistChooser()
+    }
+}
