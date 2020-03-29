337454f970a902efa558ae5264b1cffda5e03e08
Open Opus <concertino@openopus.org>
2020-03-29 00:29:20 -0300

    Suggested radio stations

---
+++ Concertino/Components/RadioStations.swift
@@ -0,0 +1,21 @@
+//
+//  RadioStations.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 28/03/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct RadioStations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RadioStations_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioStations()
+    }
+}
