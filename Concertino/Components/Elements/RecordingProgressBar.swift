a2f74c36a4d8d8507e5435c4678b8240de4c5298
Open Opus <concertino@openopus.org>
2020-04-10 22:29:15 -0300

    Reestructuring player and radio queues

---
+++ Concertino/Components/Elements/RecordingProgressBar.swift
@@ -0,0 +1,21 @@
+//
+//  RecordingProgressBar.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 10/04/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct RecordingProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordingProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordingProgressBar()
+    }
+}
---
+++ Concertino/Components/LegacyPlayer.swift
@@ -0,0 +1,21 @@
+//
+//  LegacyPlayer.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 07/04/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct LegacyPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LegacyPlayer_Previews: PreviewProvider {
+    static var previews: some View {
+        LegacyPlayer()
+    }
+}
