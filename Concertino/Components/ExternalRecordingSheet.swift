d74a1d70529d3316c8c2a260efb7d42d4c8cffcc
Open Opus <concertino@openopus.org>
2020-04-19 00:27:43 -0300

    Permalinks

---
+++ Concertino/Components/ExternalRecordingSheet.swift
@@ -0,0 +1,21 @@
+//
+//  ExternalRecordingSheet.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 18/04/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ExternalRecordingSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExternalRecordingSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        ExternalRecordingSheet()
+    }
+}
