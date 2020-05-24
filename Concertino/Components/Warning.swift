dfbf127c32901a8554a76cef1f04b6f0622d8cf8
Open Opus <concertino@openopus.org>
2020-05-23 23:56:51 -0300

    Preview player, login and radio buttons details

---
+++ Concertino/Components/Warning.swift
@@ -0,0 +1,21 @@
+//
+//  Warning.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 23/05/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct Warning: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Warning_Previews: PreviewProvider {
+    static var previews: some View {
+        Warning()
+    }
+}
