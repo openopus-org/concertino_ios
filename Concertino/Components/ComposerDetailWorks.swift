c6c97fb8ab5d9517d78c4e1e1918d523904ebe54
Open Opus <concertino@openopus.org>
2020-06-16 23:24:52 -0300

    More spacious composer page; no logo on small devs

---
+++ Concertino/Components/ComposerDetailWorks.swift
@@ -0,0 +1,21 @@
+//
+//  ComposerDetailWorks.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 16/06/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ComposerDetailWorks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ComposerDetailWorks_Previews: PreviewProvider {
+    static var previews: some View {
+        ComposerDetailWorks()
+    }
+}
---
+++ Concertino/Components/Elements/FullLoader.swift
@@ -0,0 +1,21 @@
+//
+//  FullLoader.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 16/06/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct FullLoader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FullLoader_Previews: PreviewProvider {
+    static var previews: some View {
+        FullLoader()
+    }
+}
