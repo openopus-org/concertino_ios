fdc14f9545bd34da2c8243c71f91ea89d590f667
Open Opus <concertino@openopus.org>
2020-07-23 15:43:40 -0300

    Search on a different tab; new home

---
+++ Concertino/Components/Search.swift
@@ -0,0 +1,21 @@
+//
+//  Search.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 22/07/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct Search: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Search_Previews: PreviewProvider {
+    static var previews: some View {
+        Search()
+    }
+}
