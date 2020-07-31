f5e7ca9c3e8fb4dc686d908430033f80be186f03
Open Opus <concertino@openopus.org>
2020-07-31 16:11:55 -0300

    Performer search on work detail page

---
+++ Concertino/Components/Elements/WorkSearchField.swift
@@ -0,0 +1,21 @@
+//
+//  WorkSearchField.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 31/07/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct WorkSearchField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorkSearchField_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkSearchField()
+    }
+}
