197c7ac13d37672d286763b1c0637f034b5f9f25
Open Opus <concertino@openopus.org>
2020-10-18 19:37:55 -0300

    In-app donations & new patron section on Settings

--- Concertino/Components/Settings.swift
+++ Concertino/Components/Settings.swift
@@ -20,6 +20,8 @@ struct Settings: View {
     @State private var showSignIn = false
     @State private var alreadyLogged = false
     @State private var signInLoading = false
+    @State private var donationIsLoading = true
+    @State private var donationDone = false
     @State private var inAppOffers = [SKProduct]()
     
     func loadData() {
@@ -182,7 +184,7 @@ struct Settings: View {
                                         .padding(.trailing, 4)
                                         
                                         Text("Logged-in, devices synced!")
-                                            .font(.custom("Barlow-Regular", size: 16))
+                                            .font(.custom("Barlow-Regular", size: 15))
                                     }
                                 }
                                 
@@ -198,13 +200,13 @@ struct Settings: View {
                             .font(.custom("Nunito-ExtraBold", size: 13))
                             .foregroundColor(Color(hex: 0xfe365e))
                         if #available(iOS 14.0, *) {
-                            Text("Help us keeping Concertino free! Donate and back our development and hosting costs.")
+                            Text("Help us keeping Concertino free! Donate and back our development and hosting costs. Choose a tip value below. You will be charged only once and the transaction will be processed through Apple.")
                                 .textCase(.none)
                                 .font(.custom("Barlow-Regular", size: 13))
                                 .foregroundColor(.white)
                                 .lineLimit(20)
                         } else {
-                            Text("Help us keeping Concertino free! Donate and back our development and hosting costs.")
+                            Text("Help us keeping Concertino free! Donate and back our development and hosting costs. Choose a tip value below. You will be charged only once and the transaction will be processed through Apple.")
                                 .font(.custom("Barlow-Regular", size: 13))
                                 .foregroundColor(.white)
                                 .lineLimit(20)
@@ -214,36 +216,104 @@ struct Settings: View {
                     .padding(.bottom, 16)
                 ) {
                     HStack {
-                        ForEach (self.inAppOffers.sorted { $0.price.decimalValue < $1.price.decimalValue }, id: \.self) { product in
-                            Button(
-                                action: {
-                                    SwiftyStoreKit.purchaseProduct(product, quantity: 1, atomically: true) { result in
-                                        switch result {
-                                            case .success(let purchase):
-                                                print("Purchase Success: \(purchase.productId)")
-                                            case .error(let error):
-                                                switch error.code {
-                                                    case .unknown: print("Unknown error. Please contact support")
-                                                    case .clientInvalid: print("Not allowed to make the payment")
-                                                    case .paymentCancelled: break
-                                                    case .paymentInvalid: print("The purchase identifier was invalid")
-                                                    case .paymentNotAllowed: print("The device is not allowed to make the payment")
-                                                    case .storeProductNotAvailable: print("The product is not available in the current storefront")
-                                                    case .cloudServicePermissionDenied: print("Access to cloud service information is not allowed")
-                                                    case .cloudServiceNetworkConnectionFailed: print("Could not connect to the network")
-                                                    case .cloudServiceRevoked: print("User has revoked permission to use this cloud service")
-                                                    default: print((error as NSError).localizedDescription)
+                        Spacer()
+                        
+                        if donationIsLoading {
+                            ActivityIndicator(isAnimating: donationIsLoading)
+                                .configure { $0.color = .white; $0.style = .medium }
+                        } else if donationDone {
+                            Image("favorites")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundColor(Color(hex: 0xfe365e))
+                                .frame(height: 18)
+                                .padding(.trailing, 1)
+                            
+                            Text("Thank you so much for your donation!")
+                                .font(.custom("Barlow-Regular", size: 15))
+                        } else {
+                            HStack {
+                                ForEach (self.inAppOffers.sorted { $0.price.decimalValue < $1.price.decimalValue }, id: \.self) { product in
+                                    Button(
+                                        action: {
+                                            self.donationIsLoading = true
+                                            SwiftyStoreKit.purchaseProduct(product, quantity: 1, atomically: true) { result in
+                                                self.donationIsLoading = false
+                                                switch result {
+                                                    case .success(let purchase):
+                                                        self.donationDone = true
+                                                        print("Purchase Success: \(purchase.productId)")
+                                                    case .error(let error):
+                                                        switch error.code {
+                                                            case .unknown: print("Unknown error. Please contact support")
+                                                            case .clientInvalid: print("Not allowed to make the payment")
+                                                            case .paymentCancelled: break
+                                                            case .paymentInvalid: print("The purchase identifier was invalid")
+                                                            case .paymentNotAllowed: print("The device is not allowed to make the payment")
+                                                            case .storeProductNotAvailable: print("The product is not available in the current storefront")
+                                                            case .cloudServicePermissionDenied: print("Access to cloud service information is not allowed")
+                                                            case .cloudServiceNetworkConnectionFailed: print("Could not connect to the network")
+                                                            case .cloudServiceRevoked: print("User has revoked permission to use this cloud service")
+                                                            default: print((error as NSError).localizedDescription)
+                                                        }
                                                 }
-                                        }
-                                    }
-                                },
-                                label: {
-                                    Text("\(product.localizedPrice!)")
-                                })
-                                .buttonStyle(BorderlessButtonStyle())
+                                            }
+                                        },
+                                        label: {
+                                            Text("\(product.localizedPrice!)")
+                                                .foregroundColor(.white)
+                                                .font(.custom("Nunito-Regular", size: 13))
+                                                .padding(13)
+                                                .background(Color(hex: 0xfe365e))
+                                                .cornerRadius(16)
+                                        })
+                                        .buttonStyle(BorderlessButtonStyle())
+                                }
+                            }
                         }
+                        
+                        Spacer()
                     }
                     .listRowBackground(Color.black)
+                }
+                
+                Section(header:
+                    VStack(alignment: .leading) {
+                        Text("Become our patron".uppercased())
+                            .font(.custom("Nunito-ExtraBold", size: 13))
+                            .foregroundColor(Color(hex: 0xFE365E))
+                            
+                        if #available(iOS 14.0, *) {
+                            Text("One-time tips are great, recurrent donations are amazing! Patrons receive early updates and get their names listed here.")
+                                .textCase(.none)
+                                .font(.custom("Barlow-Regular", size: 13))
+                                .foregroundColor(.white)
+                                .lineLimit(20)
+                        } else {
+                            Text("One-time tips are great, recurrent donations are amazing! Patrons receive early updates and get their names listed here.")
+                                .font(.custom("Barlow-Regular", size: 13))
+                                .foregroundColor(.white)
+                                .lineLimit(20)
+                        }
+                    }
+                    .padding(.top, 12)
+                    .padding(.bottom, 16)
+                        
+                    ){
+                        ForEach(supporters, id: \.self) { supporter in
+                            Text(supporter)
+                                .font(.custom("Nunito-Regular", size: 12))
+                                .foregroundColor(.white)
+                                .lineLimit(20)
+                        }
+                        .listRowBackground(Color.black)
+                    
+                        Button(
+                            action: { UIApplication.shared.open(URL(string: "https://patreon.com/openopus")!) },
+                            label: {
+                                SettingsMenuItem(title: "Become a patron!")
+                        })
+                        .listRowBackground(Color.black)
                 }
                 
                 Section(header:
@@ -253,14 +323,6 @@ struct Settings: View {
                     ){
                         SettingsMenuItem(title: "Version", description: AppConstants.version)
                             .listRowBackground(Color.black)
-                        /*
-                        Button(
-                            action: { UIApplication.shared.open(URL(string: "https://patreon.com/openopus")!) },
-                            label: {
-                                SettingsMenuItem(title: "Support our projects", description: "Help us keeping Concertino free! Donate and back our development and hosting costs.")
-                        })
-                            .listRowBackground(Color.black)
-                        */
                         
                         Button(
                             action: { UIApplication.shared.open(URL(string: "https://github.com/openopus-org/concertino_ios")!) },
@@ -275,17 +337,6 @@ struct Settings: View {
                                 SettingsMenuItem(title: "Find us on Twitter", description: "And tell us how has been your experience with Concertino so far!")
                         })
                             .listRowBackground(Color.black)
-                }
-                
-                Section(header:
-                    Text("Thanks for our supporters".uppercased())
-                        .font(.custom("Nunito-ExtraBold", size: 13))
-                        .foregroundColor(Color(hex: 0xFE365E))
-                    ){
-                        ForEach(supporters, id: \.self) { supporter in
-                            SettingsMenuItem(title: supporter)
-                        }
-                        .listRowBackground(Color.black)
                 }
             }
             .listStyle(GroupedListStyle())
@@ -304,6 +355,7 @@ struct Settings: View {
                     for prod in result.retrievedProducts {
                         self.inAppOffers.append(prod)
                     }
+                    self.donationIsLoading = false
                 }
             }
         })
