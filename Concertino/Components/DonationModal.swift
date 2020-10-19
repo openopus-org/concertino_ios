1f15b3f2cd3511ec956595291bc1b70753022535
Open Opus <concertino@openopus.org>
2020-10-19 19:32:11 -0300

    Donation modal

---
+++ Concertino/Components/DonationModal.swift
@@ -0,0 +1,21 @@
+//
+//  DonationModal.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 19/10/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct DonationModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DonationModal_Previews: PreviewProvider {
+    static var previews: some View {
+        DonationModal()
+    }
+}
