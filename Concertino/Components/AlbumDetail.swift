e5b2ce0da0a3add1463107062a2db05fa0d90e9a
Open Opus <concertino@openopus.org>
2020-10-26 00:06:49 -0300

    Album detail

---
+++ Concertino/Components/AlbumDetail.swift
@@ -0,0 +1,21 @@
+//
+//  AlbumDetail.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 25/10/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct AlbumDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumDetail()
+    }
+}
---
+++ Concertino/Components/Elements/RecordingDetailTracks.swift
@@ -0,0 +1,21 @@
+//
+//  RecordingDetailTracks.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 25/10/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct RecordingDetailTracks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordingDetailTracks_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordingDetailTracks()
+    }
+}
