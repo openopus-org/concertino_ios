ddd136fb11fd65e658c24f1bef421156dfa5f5d0
Open Opus <concertino@openopus.org>
2020-11-08 21:00:03 -0300

    Play buttons on album detail

---
+++ Concertino/Components/Elements/RecordingPlayButton.swift
@@ -0,0 +1,21 @@
+//
+//  RecordingPlayButton.swift
+//  Concertino
+//
+//  Created by Adriano Brandao on 08/11/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct RecordingPlayButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordingPlayButton_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordingPlayButton()
+    }
+}
